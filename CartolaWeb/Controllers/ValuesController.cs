d1da52bee285b63370c59e738b78103e437ed1f5
Fernando Mota da Silva <fmsilva@id.uff.br>
2020-05-31 22:15:48 -0300

    .netcore update

--- CartolaWeb/Controllers/ValuesController.cs
+++ CartolaWeb/Controllers/ValuesController.cs
@@ -14,197 +14,207 @@ using System.Threading.Tasks;
 
 namespace CartolaWeb.Controllers
 {
-	[Route("api/[controller]")]
-	public class ValuesController : Controller
-	{
-		// GET api/values
-		[HttpGet]
-		public IEnumerable<UsersInfo> Get()
-		{
-			var Times = GetTimesInfo().Result;
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<UsersInfo> Get()
+        {
+            List<UsersInfo> Times = GetTimesInfo()?.Result ?? null;
 
-			//por rodada, ajeitar depois TODO
-			var Escalacao = GetEscalacao(Times);
+            if (Times == null)
+            {
+                return null;
+            }
 
-			var http = (HttpWebRequest)WebRequest.Create("https://api.cartolafc.globo.com/atletas/pontuados");
-			http.ContentType = "application/json;charset=UTF-8";
-			http.UserAgent =
-				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36";
-			http.Headers.Add("X-GLB-TOKEN", Globals.Token);
+            //por rodada, ajeitar depois TODO
+            List<TimeInfo> Escalacao = GetEscalacao(Times);
 
-			var response = http.GetResponse();
+            var http = (HttpWebRequest)WebRequest.Create("https://api.cartolafc.globo.com/atletas/pontuados");
+            http.ContentType = "application/json;charset=UTF-8";
+            http.UserAgent =
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36";
+            http.Headers.Add("X-GLB-TOKEN", Globals.Token);
 
-			var stream = response.GetResponseStream();
-			var sr = new StreamReader(stream);
-			var content = sr.ReadToEnd();
+            var response = http.GetResponse();
 
-
-			foreach (var escalados in Escalacao)
-			{
-				var pontosParciais = 0.00;
-				var jogadoresAtivos = 0;
-
-				if (escalados.atletas == null)
-				{
-					Times.First(time => time.nome_cartola == escalados.DonodoTime).pontuacaoParcial = "Mongolei e não escalei meu time nessa rodada.";
-					Times.First(t => t.nome_cartola == escalados.DonodoTime).slug = jogadoresAtivos + "/12";
-					break;
-				}
-				
-				foreach (var atleta in escalados.atletas)
-				{
-					var parse = double.TryParse(GetPontuacaoParcial(content,
-						atleta.atleta_id,
-						atleta.apelido
-					), NumberStyles.Number, CultureInfo.InvariantCulture, out var pontos);
-					if (parse)
-					{
-						if (!(atleta.posicao_id == 6 && pontos == 0))
-						{
-							if (atleta.atleta_id != escalados.capitao_id)
-							{
-								pontosParciais += Math.Round(pontos, 2);
-								jogadoresAtivos++;
-							}
-							else
-							{
-								pontosParciais += 2 * Math.Round(pontos, 2);
-								jogadoresAtivos++;
-							}
-						}
-					}
-				}
+            var stream = response.GetResponseStream();
+            var sr = new StreamReader(stream);
+            var content = sr.ReadToEnd();
 
 
-				Times.First(t => t.nome_cartola == escalados.DonodoTime).pontuacaoParcial = pontosParciais.ToString("N2");
-				Times.First(t => t.nome_cartola == escalados.DonodoTime).slug = jogadoresAtivos + "/12";
+            foreach (var escalados in Escalacao)
+            {
+                var pontosParciais = 0.00;
+                var jogadoresAtivos = 0;
 
-			}
-			return Times.ToArray();
-		}
+                if (escalados.atletas == null)
+                {
+                    Times.First(time => time.nome_cartola == escalados.DonodoTime).pontuacaoParcial = "Mongolei e não escalei meu time nessa rodada.";
+                    Times.First(t => t.nome_cartola == escalados.DonodoTime).slug = jogadoresAtivos + "/12";
+                    break;
+                }
 
-		private static List<TimeInfo> GetEscalacao(List<UsersInfo> Times)
-		{
-			var ListEscalacao = new List<TimeInfo>();
-			foreach (var time in Times)
-			{
-				var http = (HttpWebRequest)WebRequest.Create("https://api.cartolafc.globo.com/time/slug/" + time.slug);
-				http.ContentType = "application/json;charset=UTF-8";
-				http.UserAgent =
-					"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36";
-				http.Headers.Add("X-GLB-TOKEN",
-					"17f947b9200b2a803a412555b56b8ea166574596f41384d593974795a30586433395662557561316b4b52336a306b486e75576772456d4a6539694b57303439524257724d373932443658574a354873416b4f79644c54796e6968637871785a43674375625f673d3d3a303a706f63616f323030352e32303136");
-
-
-				var response = http.GetResponse();
-
-				var stream = response.GetResponseStream();
-				var sr = new StreamReader(stream);
-				var content = sr.ReadToEnd();
-
-				var escalacao = JsonConvert.DeserializeObject<TimeInfo>(content);
-				escalacao.DonodoTime = time.nome_cartola;
-				ListEscalacao.Add(escalacao);
-			};
-			return ListEscalacao;
-		}
-
-		private static async Task<List<UsersInfo>> GetTimesInfo()
-		{
-			var http = (HttpWebRequest)WebRequest.Create("https://api.cartolafc.globo.com/auth/liga/liga-carioca-capixaba");
-			http.ContentType = "application/json;charset=UTF-8";
-			http.UserAgent =
-				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36";
-			http.Headers.Add("X-GLB-TOKEN", Globals.Token);
-
-			try
-			{
-				var response = http.GetResponse();
-
-				var stream = response.GetResponseStream();
-				var sr = new StreamReader(stream);
-				var content = sr.ReadToEnd();
-
-				var Liga = JsonConvert.DeserializeObject<LigaInfo>(content);
+                foreach (var atleta in escalados.atletas)
+                {
+                    var parse = double.TryParse(GetPontuacaoParcial(content,
+                        atleta.atleta_id,
+                        atleta.apelido
+                    ), NumberStyles.Number, CultureInfo.InvariantCulture, out var pontos);
+                    if (parse)
+                    {
+                        if (!(atleta.posicao_id == 6 && pontos == 0))
+                        {
+                            if (atleta.atleta_id != escalados.capitao_id)
+                            {
+                                pontosParciais += Math.Round(pontos, 2);
+                                jogadoresAtivos++;
+                            }
+                            else
+                            {
+                                pontosParciais += 2 * Math.Round(pontos, 2);
+                                jogadoresAtivos++;
+                            }
+                        }
+                    }
+                }
 
 
-				var Times = new List<UsersInfo>();
+                Times.First(t => t.nome_cartola == escalados.DonodoTime).pontuacaoParcial = pontosParciais.ToString("N2");
+                Times.First(t => t.nome_cartola == escalados.DonodoTime).slug = jogadoresAtivos + "/12";
 
-				foreach (var time in Liga.times)
-				{
-					Times.Add(new UsersInfo(time.nome, time.nome_cartola, time.patrimonio, time.ranking, time.pontos, time.slug));
-				}
+            }
+            return Times.ToArray();
+        }
 
-				return Times;
-
-			}
-
-			catch
-			{
-				GetAuthenticationToken().Wait();
-				return await GetTimesInfo();
-			}
-		}
-
-		private static async Task GetAuthenticationToken()
-		{
-			var httpNew = (HttpWebRequest)WebRequest.Create("https://login.globo.com/api/authentication");
-			httpNew.ContentType = "application/json;charset=UTF-8";
-			httpNew.UserAgent =
-				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36";
-			httpNew.Method = "POST";
-
-			ServicePointManager.SecurityProtocol =
-				SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+        private static List<TimeInfo> GetEscalacao(List<UsersInfo> Times)
+        {
+            var ListEscalacao = new List<TimeInfo>();
+            foreach (var time in Times)
+            {
+                var http = (HttpWebRequest)WebRequest.Create("https://api.cartolafc.globo.com/time/slug/" + time.slug);
+                http.ContentType = "application/json;charset=UTF-8";
+                http.UserAgent =
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36";
+                http.Headers.Add("X-GLB-TOKEN",
+                    "17f947b9200b2a803a412555b56b8ea166574596f41384d593974795a30586433395662557561316b4b52336a306b486e75576772456d4a6539694b57303439524257724d373932443658574a354873416b4f79644c54796e6968637871785a43674375625f673d3d3a303a706f63616f323030352e32303136");
 
 
-			var payload = new AuthenticationPayload
-			{
-				payload = new Payload
-				{
-					email = "pocao2005@gmail.com",
-					password = "asdf1234",
-					serviceId = 438
-				}
-			};
+                var response = http.GetResponse();
 
-			string json = JsonConvert.SerializeObject(payload);
+                var stream = response.GetResponseStream();
+                var sr = new StreamReader(stream);
+                var content = sr.ReadToEnd();
 
-			var client = new HttpClient();
-			var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+                var escalacao = JsonConvert.DeserializeObject<TimeInfo>(content);
+                escalacao.DonodoTime = time.nome_cartola;
+                ListEscalacao.Add(escalacao);
+            };
+            return ListEscalacao;
+        }
 
-			var response = await client.PostAsync("https://login.globo.com/api/authentication", httpContent);
+        private static async Task<List<UsersInfo>> GetTimesInfo()
+        {
+            var http = (HttpWebRequest)WebRequest.Create("https://api.cartolafc.globo.com/auth/liga/liga-carioca-capixaba");
+            http.ContentType = "application/json;charset=UTF-8";
+            http.UserAgent =
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36";
+            http.Headers.Add("X-GLB-TOKEN", Globals.Token);
 
-			if (response.Content != null)
-			{
-				var responseContent = await response.Content.ReadAsStringAsync();
-				Globals.Token = JsonConvert.DeserializeObject<AuthenticationInfo>(responseContent).glbId;
-			}
+            try
+            {
+                var response = http.GetResponse();
 
-		}
+                var stream = response.GetResponseStream();
+                var sr = new StreamReader(stream);
+                var content = sr.ReadToEnd();
+
+                var Liga = JsonConvert.DeserializeObject<LigaInfo>(content);
 
 
-		public string GetPontuacaoParcial(string content, int id, string apelido)
-		{
-			try
-			{
-				var jogador = string.Format("\"{0}\":{{\"apelido\":\"{1}\",\"pontuacao\":", id, apelido);
-				var limiteInicial = content.IndexOf(jogador) + jogador.Length;
-				var limiteFinal = content.IndexOf(',', limiteInicial);
+                var Times = new List<UsersInfo>();
 
-				var pontuacao = content.Substring(limiteInicial, limiteFinal - limiteInicial).Trim();
+                if (Liga == null || Liga.times == null)
+                {
+                    return null;
+                }
 
-				if (pontuacao != null)
-				{
-					return pontuacao;
-				}
+                foreach (var time in Liga.times)
+                {
+                    Times.Add(new UsersInfo(time.nome, time.nome_cartola, time.patrimonio, time.ranking, time.pontos, time.slug));
+                }
 
-				return "0.00";
-			}
-			catch
-			{
-				return "0.00";
-			}
-		}
-	}
+                return Times;
+
+            }
+
+            catch
+            {
+                GetAuthenticationToken().Wait();
+                return await GetTimesInfo();
+            }
+        }
+
+        private static async Task GetAuthenticationToken()
+        {
+            var httpNew = (HttpWebRequest)WebRequest.Create("https://login.globo.com/api/authentication");
+            httpNew.ContentType = "application/json;charset=UTF-8";
+            httpNew.UserAgent =
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36";
+            httpNew.Method = "POST";
+
+            ServicePointManager.SecurityProtocol =
+                SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+
+            var payload = new AuthenticationPayload
+            {
+                payload = new Payload
+                {
+                    email = "pocao2005@gmail.com",
+                    password = "asdf1234",
+                    serviceId = 438
+                }
+            };
+
+            string json = JsonConvert.SerializeObject(payload);
+
+            var client = new HttpClient();
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("https://login.globo.com/api/authentication", httpContent);
+
+            if (response.Content != null)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                Globals.Token = JsonConvert.DeserializeObject<AuthenticationInfo>(responseContent).glbId;
+            }
+
+        }
+
+
+        public string GetPontuacaoParcial(string content, int id, string apelido)
+        {
+            try
+            {
+                var jogador = string.Format("\"{0}\":{{\"apelido\":\"{1}\",\"pontuacao\":", id, apelido);
+                var limiteInicial = content.IndexOf(jogador) + jogador.Length;
+                var limiteFinal = content.IndexOf(',', limiteInicial);
+
+                var pontuacao = content.Substring(limiteInicial, limiteFinal - limiteInicial).Trim();
+
+                if (pontuacao != null)
+                {
+                    return pontuacao;
+                }
+
+                return "0.00";
+            }
+            catch
+            {
+                return "0.00";
+            }
+        }
+    }
 }
